4033069a61bb94bb5f9e11ac6ed743a5711a657a
Coleman Imhoff <colemanimhoff@gmail.com>
2018-05-01 10:44:08 -0600

    deployed to heroku

---
+++ cohorts.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        "id": 1,
+        "cohortName": "17-01-WD-DP",
+        "code": "g100",
+        "numberOfStudents": 28
+    },
+    {
+        "id": 2,
+        "cohortName": "DS-GT,g105",
+        "code": "g105",
+        "numberOfStudents": 24
+    },
+    {
+        "id": 3,
+        "cohortName": "WD-PX,g109",
+        "code": "109",
+        "numberOfStudents": 30
+    },
+    {
+        "id": 4,
+        "cohortName": "WD-BD,g110",
+        "code": "110",
+        "numberOfStudents": 29
+    }
+]
---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = parseInt(process.env.PORT || 3000)
+
+const cohorts = require('./cohorts.js')
+
+function getDataById(data, id) {
+    return data.filter(cohort => cohort.id == id)
+}
+
+app.get('/', (request, response) => response.json(cohorts))
+
+app.get('/:id', (request, response) => {
+    let filteredCohort = getDataById(cohorts, request.params.id)
+    if (filteredCohort.length < 1) {
+        response.status(404).json({
+            error: { message: 'No record found!' }
+        })
+    } else {
+        response.json({ cohort: filteredCohort })
+    }
+})
+
+app.listen(port)
+    .on('error', console.error.bind(console))
+    .on('listening', console.log.bind(console, 'Listening on ' + port));
